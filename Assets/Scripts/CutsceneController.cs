4d100098b938d425d13e028d55ff99434ae49051
attevinon <violet.lutsevich@gmail.com>
2025-04-27 14:21:22 +0300

    cutscene logic

--- Assets/Scripts/CutsceneController.cs
+++ Assets/Scripts/CutsceneController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using DG.Tweening;
+using Scripts.Data;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,66 +11,55 @@ namespace Scripts
     {
         public event Action StartShown;
         public event Action EndShown;
-        //start config
-        //end config
 
-        private bool _isStart;
-        private bool _isEnd;
+        [SerializeField] private CutsceneConfig _startConfig;
+        [SerializeField] private CutsceneConfig _endConfig;
 
         [SerializeField] private float _fadeDuration;
         [SerializeField] private CanvasGroup _canvasGroup;
         [SerializeField] private Button _button;
         [SerializeField] private TMP_Text _line;
+        [SerializeField] private float _textFadeDuration = 0.2f;
+        
+        private bool _isStart;
+        private bool _isEnd;
+        private int _index;
 
         private void Awake()
         {
             _canvasGroup.enabled = true;
+            _button.gameObject.SetActive(false);
         }
 
         private void OnEnable()
         {
-            _button.onClick.AddListener(NextLine);
+            _button.onClick.AddListener(ProcessClick);
         }
 
         private void OnDisable()
         {
-            _button.onClick.RemoveListener(NextLine);
+            _button.onClick.RemoveListener(ProcessClick);
         }
 
         public void ShowStartCutscene()
         {
             _isStart = true;
-            _canvasGroup
-                .DOFade(0f, _fadeDuration)
-                .SetDelay(5f)
-                .SetLink(gameObject)
-                .OnComplete(ShowComplete)
-                .Play();
+            _index = 0;
+            SetText(_startConfig.Lines[_index]);
         }
         
         public void ShowEndCutscene()
         {
             _isEnd = true;
+            _index = 0;
             EnableThis(true);
             _canvasGroup
                 .DOFade(1f, _fadeDuration)
                 .SetLink(gameObject)
-                .OnComplete(() =>
-                {
-                    _isEnd = false;
-                    EndShown?.Invoke();
-
-                })
+                .OnComplete(() => SetText(_endConfig.Lines[_index]))
                 .Play();
         }
-
-        private void EnableThis(bool enable)
-        {
-            _button.enabled = enable;
-            _canvasGroup.interactable = enable;
-            _canvasGroup.blocksRaycasts = enable;
-        }
-
+        
         private void ShowComplete()
         {
             _isStart = false;
@@ -77,14 +67,62 @@ namespace Scripts
             StartShown?.Invoke();
         }
 
-        private void NextLine()
+        private void EnableThis(bool enable)
         {
-            
+            _canvasGroup.interactable = enable;
+            _canvasGroup.blocksRaycasts = enable;
         }
 
-        private void SetText(string text)
+        private void ProcessClick()
         {
-            _line.text = text;
+            if(_line.color.a > 0.98f)
+                NextLine();
+        }
+
+        private void NextLine()
+        {
+            _index++;
+            if (_isStart)
+            {
+                if (_index >= _startConfig.Lines.Length)
+                {
+                    SetText(string.Empty, false);
+                    _canvasGroup
+                        .DOFade(0f, _fadeDuration)
+                        .SetDelay(1f)
+                        .SetLink(gameObject)
+                        .OnComplete(ShowComplete)
+                        .Play();
+                    return;
+                }
+                
+                SetText(_startConfig.Lines[_index]);
+            }
+            else if (_isEnd)
+            {
+                if (_index == _endConfig.Lines.Length)
+                {
+                    _isEnd = false;
+                    _button.gameObject.SetActive(false);
+                    EndShown?.Invoke();
+                    return;
+                }
+                
+                SetText(_endConfig.Lines[_index]);
+            }
+        }
+
+        private void SetText(string text, bool enableButton = true)
+        {
+            _button.gameObject.SetActive(false);
+            _line
+                .DOFade(0.5f, _textFadeDuration)
+                .SetDelay(0.02f)
+                .SetLoops(2, LoopType.Yoyo)
+                .OnStepComplete(() => _line.text = text)
+                .OnComplete(() => _button.gameObject.SetActive(enableButton))
+                .SetLink(gameObject)
+                .Play();
         }
     }
 }
---
+++ Assets/Scripts/Gameplay/Data/CutsceneConfig.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Scripts.Data
+{
+    [CreateAssetMenu(fileName = "CutsceneConfig", menuName = "Radunica/CutsceneConfig")]
+    public class CutsceneConfig : ScriptableObject
+    {
+        [SerializeField] private string[] _lines;
+        public string[] Lines => _lines;
+    }
+}
