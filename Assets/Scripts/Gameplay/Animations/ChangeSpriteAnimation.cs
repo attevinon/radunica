e0da92b65e6d55dd1f40ff27800f3fa559be4472
attevinon <violet.lutsevich@gmail.com>
2026-04-21 12:48:58 +0300

    better flowers watering logic

--- Assets/Scripts/Gameplay/Animations/ChangeSpriteAnimation.cs
+++ Assets/Scripts/Gameplay/Animations/ChangeSpriteAnimation.cs
@@ -1,48 +1,43 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Scripts.Animations
 {
     public class ChangeSpriteAnimation : ScaleAnimation, IAnimatableDuringInput
     {
-        [SerializeField] private SpriteRenderer _spriteRenderer;
-        [SerializeField] private Sprite _newSprite;
+        [FormerlySerializedAs("_spriteRenderer")] [SerializeField] private SpriteRenderer _drySpriteRenderer; 
+        [SerializeField] private SpriteRenderer _wetSpriteRenderer;
         private bool _isInAnimation;
         private bool _spriteChanged;
 
+        private void Awake()
+        {
+            Color color = _wetSpriteRenderer.color;
+            color.a = 0f;
+            _wetSpriteRenderer.color = color;
+        }
+
         public void AnimateDuringInput(float progress)
         {
-            if (progress < 0.5f)
-            {
-                ChangeAlpha(progress / 0.5f, 0.5f);
-            }
-            else
-            {
-                if (!_spriteChanged)
-                {
-                    _spriteChanged = true;
-                    _spriteRenderer.sprite = _newSprite;
-                }
-                
-                ChangeAlpha((progress - 0.5f) / 0.5f, 1f);
-            }
+            ChangeAlpha(_wetSpriteRenderer, progress, 1f);
         }
 
         public override void Animate(Action callback)
         {
-            Color color = _spriteRenderer.color;
+            Color color = _drySpriteRenderer.color;
             color.a = 1f;
-            _spriteRenderer.color = color;
+            _drySpriteRenderer.color = color;
             base.Animate(callback);
         }
         
-        public void ChangeAlpha(float progress, float targetAlpha)
+        private void ChangeAlpha(SpriteRenderer spriteRenderer, float progress, float targetAlpha)
         {
-            float startAlpha = targetAlpha > 0.98f ? 0.5f : 1f; 
-            Color color = _spriteRenderer.color;
+            float startAlpha = spriteRenderer.color.a;
+            Color color = spriteRenderer.color;
             float alpha = Mathf.Lerp(startAlpha, targetAlpha, progress);
             color.a = alpha;
-            _spriteRenderer.color = color;
+            spriteRenderer.color = color;
         }
     }
 }
